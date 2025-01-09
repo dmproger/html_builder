0f0ef57165fd1619d50cb10b0ee6fd7829ecf9b3
Dmitry Programmer <dmproger@gmail.com>
2025-01-09 23:40:10 +0300

    default-dsl-with-body-param

--- lib/html/dsl.rb
+++ lib/html/dsl.rb
@@ -1,6 +1,13 @@
 class HTML
   module Dsl
-    METHODS = %w(h1 div p svg rect)
+    # todo remove and onfly with method_missing
+    METHODS = %w(h1 div p svg rect) 
+
+    for method in METHODS
+      define_method(method) do |body = nil|
+        { body: }
+      end
+    end
 
     def svg(width, height)
       { width:, height:, xmlns: "http://www.w3.org/2000/svg" }
@@ -8,18 +15,6 @@ class HTML
 
     def rect(width, height, x, y)
       { width:, height:, x:, y:}
-    end
-
-    def div(body = nil)
-      { body: }
-    end
-
-    def h1(body = nil)
-      { body: }
-    end
-
-    def p(body = nil)
-      { body: }
     end
   end
 end
