7088acc8d9a80bd0db0a045ca48f0d0c63df8efe
Dmitry Programmer <dmproger@gmail.com>
2025-01-09 23:55:34 +0300

    fix

--- lib/html.rb
+++ lib/html.rb
@@ -17,7 +17,7 @@ class HTML
       define_method(tag) do |*args, **attrs, &block|
         @root ||= __method__
 
-        result = Tag.call(__method__, **attrs.merge(super(*args)).merge(body: block&.call))
+        result = Tag.call(__method__, **attrs.merge(super(*args)).merge({ body: block&.call }.compact))
         return result unless @root == __method__
 
         @buffer ? @buffer += result : @stdout.puts(result)
